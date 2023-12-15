ce20eb871cd7ce005ac150180a36422509bda0fe
Joshua Holmes <joshua.phillip.holmes@gmail.com>
2023-12-15 00:18:19 -0800

    Refactor shaders

--- src/gl.rs
+++ src/gl.rs
@@ -1,4 +1,4 @@
-use std::error::{Error, self};
+use std::{error::{Error, self}, fmt::Display};
 
 use gl33::{
     self,
@@ -15,6 +15,36 @@ pub enum BufferType {
     Array = gl33::GL_ARRAY_BUFFER.0 as isize,
     /// Element Array Buffers hold indexes of what vertexes to use for drawing.
     ElementArray = gl33::GL_ELEMENT_ARRAY_BUFFER.0 as isize,
+}
+
+pub enum ShaderType {
+    Vertex,
+    Fragment,
+}
+impl Display for ShaderType {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let name = match self {
+            Vertex => "Vertex",
+            Fragment => "Fragment",
+        };
+
+        write!(f, "{}", name)?;
+
+        Ok(())
+    }
+}
+impl ShaderType {
+    pub fn to_glenum(&self) -> gl33::GLenum {
+        match self {
+            Vertex => gl33::GL_VERTEX_SHADER,
+            Fragment => gl33::GL_FRAGMENT_SHADER,
+        }
+    }
+}
+
+pub enum LogType {
+    Shader,
+    Program,
 }
 
 pub struct Gl {
@@ -77,6 +107,114 @@ impl Gl {
             );
         }
     }
+
+    pub fn vertex_attrib_pointer(&self, index: u32, size: i32, data_type: gl33::GLenum, normalized: bool, stride: usize) {
+        unsafe {
+            self.gl_fns.VertexAttribPointer(index, size, data_type, normalized as u8, stride.try_into().unwrap(), 0 as *const _);
+        }
+    }
+
+    pub fn enable_vertex_attrib_array(&self, index: u32) {
+        unsafe { self.gl_fns.EnableVertexAttribArray(index) }
+    }
+
+    pub fn create_program(&self) -> u32 {
+        unsafe { self.gl_fns.CreateProgram() }
+    }
+
+    pub fn create_shader(&self, shader_type: ShaderType) -> Result<u32, &str> {
+        let shader = unsafe { self.gl_fns.CreateShader(shader_type.to_glenum()) };
+        if shader == 0 {
+            Err::<u32, _>(format!("Could not create shader of type: {}", shader_type).as_str());
+        }
+        Ok(shader)
+    }
+
+    pub fn shader_source(&self, shader_data: u32, count: i32, source_code: String) {
+        unsafe {
+            self.gl_fns.ShaderSource(
+                shader_data,
+                count,
+                &(source_code.as_bytes().as_ptr().cast()),
+                &(source_code.len().try_into().unwrap())
+            )
+        }
+    }
+
+    fn compile_shader(&self, shader_data: u32) {
+        unsafe { self.gl_fns.CompileShader(shader_data) }
+    }
+
+    pub fn get_program_iv(&self, shader_program: u32) -> Result<(), &str> {
+        let mut success = 0;
+        unsafe { self.gl_fns.GetProgramiv(shader_program, gl33::GL_LINK_STATUS, &mut success) }
+        if success == 0 {
+            Err(self.get_info_log(LogType::Program, shader_program))
+        } else {
+            Ok(())
+        }
+    }
+
+    fn get_shader_iv(&self, shader_data: u32) -> Result<(), &str> {
+        let mut success = 0;
+        unsafe { self.gl_fns.GetShaderiv(shader_data, gl33::GL_COMPILE_STATUS, &mut success); }
+        if success == 0 {
+            Err(self.get_info_log(LogType::Shader, shader_data))
+        } else {
+            Ok(())
+        }
+    }
+
+    fn get_info_log(&self, log_type: LogType, shader_data: u32) -> &str {
+        let mut v: Vec<u8> = Vec::with_capacity(1024);
+        let mut log_len = 0;
+        match log_type {
+            LogType::Shader => unsafe {
+                self.gl_fns.GetShaderInfoLog(
+                    shader_data,
+                    1024,
+                    &mut log_len,
+                    v.as_mut_ptr().cast()
+                )
+            }
+            LogType::Program => unsafe {
+                self.gl_fns.GetProgramInfoLog(
+                    shader_data,
+                    1024,
+                    &mut log_len,
+                    v.as_mut_ptr().cast()
+                )
+            }
+        }
+        unsafe {
+            v.set_len(log_len.try_into().unwrap());
+        }
+        String::from_utf8_lossy(&v).to_string().as_str()
+    }
+
+    fn attach_shader(&self, shader_program: u32, shader_data: u32) {
+        unsafe { self.gl_fns.AttachShader(shader_program, shader_data) }
+    }
+
+    pub fn link_program(&self, shader_program: u32) {
+        unsafe { self.gl_fns.LinkProgram(shader_program) }
+    }
+
+    fn delete_shader(&self, shader_data: u32) {
+        unsafe { self.gl_fns.DeleteShader(shader_data) }
+    }
+
+    pub fn use_program(&self, shader_program: u32) {
+        unsafe { self.gl_fns.UseProgram(shader_program) }
+    }
+
+    pub fn clear(&self) {
+        unsafe { self.gl_fns.Clear(gl33::GL_COLOR_BUFFER_BIT) }
+    }
+
+    pub fn draw_arrays(&self, index: usize, count: i32) {
+        unsafe { self.gl_fns.DrawArrays(gl33::GL_TRIANGLES, index as i32, count) }
+    }
 }
 
 /// Basic wrapper for a [Vertex Array
@@ -135,5 +273,61 @@ impl<'a> Buffer<'a> {
             }
             None => Err("Could not set buffer data, buffer type not found")
         }
+    }
+}
+
+pub struct ShaderOpts<'a> {
+    pub name_str: String,
+    pub shader_type: ShaderType,
+    pub source_code: &'a str,
+}
+impl<'a> ShaderOpts<'a> {
+    pub fn new(shader_type: ShaderType, source_code: &str) -> Self {
+        Self {
+            name_str: format!("{}", shader_type),
+            shader_type,
+            source_code
+        }
+    }
+
+    pub fn from(iter: impl Iterator<Item = (ShaderType, &'a str)>) -> Vec<Self> {
+        let mut opts = Vec::new();
+        for (shader_type, source_code) in iter {
+            opts.push(Self::new(shader_type, source_code));
+        }
+        opts
+    }
+}
+
+pub struct Shader<'a> {
+    shader_type: ShaderType,
+    shader_data: u32,
+    gl: &'a Gl,
+}
+impl<'a> Shader<'a> {
+    pub fn new(shader_type: ShaderType, source_code: String, gl: &'a Gl) -> Result<Self, &'static str> {
+        let shader_data = gl.create_shader(shader_type)?;
+        gl.shader_source(shader_data, 1, source_code);
+        Ok(Self {
+            shader_type,
+            shader_data,
+            gl,
+        })
+    }
+
+    pub fn compile(&self) {
+        self.gl.compile_shader(self.shader_data);
+    }
+
+    pub fn get_shader_iv(&self) -> Result<(), &str> {
+        self.gl.get_shader_iv(self.shader_data)
+    }
+
+    pub fn attach(&self, shader_program: u32) {
+        self.gl.attach_shader(shader_program, self.shader_data);
+    }
+
+    pub fn delete(&self) {
+        self.gl.delete_shader(self.shader_data);
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,7 @@ use beryllium::{
 mod gl;
 mod window;
 
-use gl::{Gl, Buffer, BufferType};
+use gl::{Gl, Buffer, BufferType, ShaderType, Shader};
 
 use crate::gl::VertexArray;
 
@@ -23,18 +23,18 @@ const VERTICES: [Vertex; 3] = [
 const WINDOW_TITLE: &str = "My triangle";
 const WINDOW_WIDTH: i32 = 800;
 const WINDOW_HEIGHT: i32 = 600;
-const VERT_SHADER: &str = r#"#version 330 core
+const VERT_SHADER: String = String::from(r#"#version 330 core
 layout (location = 0) in vec3 pos;
 void main() {
     gl_Position = vec4(pos.x, pos.y, pos.z, 1.0);
 }
-"#;
-const FRAG_SHADER: &str = r#"#version 330 core
+"#);
+const FRAG_SHADER: String = String::from(r#"#version 330 core
 out vec4 final_color;
 void main() {
     final_color = vec4(1.0, 0.5, 0.2, 1.0);
 }
-"#;
+"#);
 
 fn main() -> Result<(), &'static str> {
     let win = window::create_gl_window_obj(WINDOW_TITLE, WINDOW_WIDTH, WINDOW_HEIGHT);
@@ -53,79 +53,39 @@ fn main() -> Result<(), &'static str> {
             gl33::GL_STATIC_DRAW,
         )?;
 
-        gl.VertexAttribPointer(
+        gl.vertex_attrib_pointer(
             0,
             3,
             gl33::GL_FLOAT,
-            gl33::GL_FALSE.0 as u8,
-            mem::size_of::<Vertex>().try_into().unwrap(),
-            0 as *const _
+            false,
+            mem::size_of::<Vertex>(),
         );
-        gl.EnableVertexAttribArray(0);
+        gl.enable_vertex_attrib_array(0);
 
-        let shader_opts = [
-            ("Vertex", gl33::GL_VERTEX_SHADER, VERT_SHADER),
-            ("Fragment", gl33::GL_FRAGMENT_SHADER, FRAG_SHADER)
+        let shader_program = gl.create_program();
+        let shaders = [
+            Shader::new(ShaderType::Vertex, VERT_SHADER, &gl)?,
+            Shader::new(ShaderType::Fragment, FRAG_SHADER, &gl)?,
         ];
-        let mut shaders = Vec::with_capacity(shader_opts.len());
-
-        let shader_program = gl.CreateProgram();
-        for (shader_log_name, shader_type, source_code) in shader_opts {
-            let shader = gl.CreateShader(shader_type);
-            assert_ne!(shader, 0);
-
-            gl.ShaderSource(
-                shader,
-                1,
-                &(source_code.as_bytes().as_ptr().cast()),
-                &(source_code.len().try_into().unwrap())
-            );
-            gl.CompileShader(shader);
-
-            let mut success = 0;
-            gl.GetShaderiv(shader, gl33::GL_COMPILE_STATUS, &mut success);
-
-            if success == 0 {
-                let mut v: Vec<u8> = Vec::with_capacity(1024);
-                let mut log_len = 0;
-                gl.GetShaderInfoLog(
-                    shader,
-                    1024,
-                    &mut log_len,
-                    v.as_mut_ptr().cast()
-                );
-                v.set_len(log_len.try_into().unwrap());
-                panic!("{} Compile Error: {}", shader_log_name, String::from_utf8_lossy(&v));
-            }
-
-            gl.AttachShader(shader_program, shader);
-            shaders.push(shader);
-        } // shader for loop
-        gl.LinkProgram(shader_program);
-
-        let mut success = 0;
-        gl.GetProgramiv(shader_program, gl33::GL_LINK_STATUS, &mut success);
-        if success == 0 {
-            let mut v: Vec<u8> = Vec::with_capacity(1024);
-            let mut log_len = 0_i32;
-            gl.GetProgramInfoLog(
-                shader_program,
-                1024,
-                &mut log_len,
-                v.as_mut_ptr().cast()
-            );
-            v.set_len(log_len.try_into().unwrap());
-            panic!("Program Link Error: {}", String::from_utf8_lossy(&v));
-        }
 
         for shader in shaders {
-            gl.DeleteShader(shader);
+            shader.compile();
+            shader.get_shader_iv()?;
+            shader.attach(shader_program);
         }
-        gl.UseProgram(shader_program);
+
+        gl.link_program(shader_program);
+        gl.get_program_iv(shader_program);
+
+        for shader in shaders {
+            shader.delete();
+        }
+
+        gl.use_program(shader_program);
 
         win.set_swap_interval(video::GlSwapInterval::Vsync).unwrap();
 
-        gl.Clear(gl33::GL_COLOR_BUFFER_BIT);
+        gl.clear();
 
         'main_loop: loop {
             // handle events this frame
@@ -138,8 +98,8 @@ fn main() -> Result<(), &'static str> {
             // now the events are clear
 
             // here's where we could change the world state and draw
-            gl.Clear(gl33::GL_COLOR_BUFFER_BIT);
-            gl.DrawArrays(gl33::GL_TRIANGLES, 0, 3);
+            gl.clear();
+            gl.draw_arrays(0, 3);
 
             win.swap_window();
         }
